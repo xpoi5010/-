dad3e901590e35b6d661f367c50aafacb69f794c
Yuan <xpoi5010@gmail.com>
2018-05-20 22:11:46 +0800

    First Version
    猜數字初版

---
+++ 猜數字/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace 猜數字
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.d1 = new System.Windows.Forms.TextBox();
+            this.d2 = new System.Windows.Forms.TextBox();
+            this.d3 = new System.Windows.Forms.TextBox();
+            this.d4 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.開始遊戲ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // d1
+            // 
+            this.d1.Font = new System.Drawing.Font("微軟正黑體", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.d1.Location = new System.Drawing.Point(3, 3);
+            this.d1.MaxLength = 1;
+            this.d1.Name = "d1";
+            this.d1.Size = new System.Drawing.Size(83, 93);
+            this.d1.TabIndex = 0;
+            this.d1.Text = "5";
+            this.d1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.d1.TextChanged += new System.EventHandler(this.d_TextChanged);
+            this.d1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.d_KeyDown);
+            // 
+            // d2
+            // 
+            this.d2.Font = new System.Drawing.Font("微軟正黑體", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.d2.Location = new System.Drawing.Point(92, 3);
+            this.d2.MaxLength = 1;
+            this.d2.Name = "d2";
+            this.d2.Size = new System.Drawing.Size(83, 93);
+            this.d2.TabIndex = 1;
+            this.d2.Text = "7";
+            this.d2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.d2.TextChanged += new System.EventHandler(this.d_TextChanged);
+            this.d2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.d_KeyDown);
+            // 
+            // d3
+            // 
+            this.d3.Font = new System.Drawing.Font("微軟正黑體", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.d3.Location = new System.Drawing.Point(181, 3);
+            this.d3.MaxLength = 1;
+            this.d3.Name = "d3";
+            this.d3.Size = new System.Drawing.Size(83, 93);
+            this.d3.TabIndex = 2;
+            this.d3.Text = "4";
+            this.d3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.d3.TextChanged += new System.EventHandler(this.d_TextChanged);
+            this.d3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.d_KeyDown);
+            // 
+            // d4
+            // 
+            this.d4.Font = new System.Drawing.Font("微軟正黑體", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.d4.Location = new System.Drawing.Point(270, 3);
+            this.d4.MaxLength = 1;
+            this.d4.Name = "d4";
+            this.d4.Size = new System.Drawing.Size(83, 93);
+            this.d4.TabIndex = 3;
+            this.d4.Text = "3";
+            this.d4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.d4.TextChanged += new System.EventHandler(this.d_TextChanged);
+            this.d4.KeyDown += new System.Windows.Forms.KeyEventHandler(this.d_KeyDown);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.d1);
+            this.panel1.Controls.Add(this.d4);
+            this.panel1.Controls.Add(this.d2);
+            this.panel1.Controls.Add(this.d3);
+            this.panel1.Location = new System.Drawing.Point(12, 53);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(362, 98);
+            this.panel1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(12, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(362, 40);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "猜測";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.Control;
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listView1.Font = new System.Drawing.Font("微軟正黑體", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.listView1.FullRowSelect = true;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(392, 24);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(408, 426);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "0";
+            this.columnHeader1.Width = 208;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "1";
+            this.columnHeader2.Width = 199;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.開始遊戲ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 開始遊戲ToolStripMenuItem
+            // 
+            this.開始遊戲ToolStripMenuItem.Name = "開始遊戲ToolStripMenuItem";
+            this.開始遊戲ToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.開始遊戲ToolStripMenuItem.Text = "開始遊戲";
+            this.開始遊戲ToolStripMenuItem.Click += new System.EventHandler(this.開始遊戲ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "猜數字";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox d1;
+        private System.Windows.Forms.TextBox d2;
+        private System.Windows.Forms.TextBox d3;
+        private System.Windows.Forms.TextBox d4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 開始遊戲ToolStripMenuItem;
+    }
+}
+
---
+++ 猜數字/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+namespace 猜數字
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InputNumber = "0000";
+            Disable();
+        }
+        string Number = "";
+        public string InputNumber
+        {
+            get
+            {
+                return $@"{d1.Text}{d2.Text}{d3.Text}{d4.Text}";
+            }
+            set
+            {
+                string val = value;
+                char[] cha = val.ToCharArray();
+                for(int i = 1; i <= cha.Count(); i++)
+                {
+                    panel1.Controls[$"d{i}"].Text = cha[i-1].ToString();
+                }
+            }
+        }
+        bool GameEnabled = false;
+        public void Enable()
+        {
+
+            RandomNumber();
+            button1.Enabled = true;
+            GameEnabled = true;
+            foreach (Control cl in panel1.Controls)
+            {
+                cl.Enabled = true;
+            }
+        }
+        public void Disable()
+        {
+            button1.Enabled = false;
+            GameEnabled = false;
+            listView1.Items.Clear();
+            InputNumber = "0000";
+            foreach(Control cl in panel1.Controls)
+            {
+                cl.Enabled = false;
+            }
+        }
+        private void 開始遊戲ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (GameEnabled && MessageBox.Show("目前仍有遊戲進行中，您確定要重新開始?","詢問",MessageBoxButtons.YesNo,MessageBoxIcon.Question) == DialogResult.No)
+            {
+                return;
+            }
+            Disable();
+            Enable();
+        }
+        private void d_TextChanged(object sender, EventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox.Text.Length == 1 && textBox.TabIndex<3)
+            {
+                TextBox text = (TextBox)panel1.Controls[$"d{textBox.TabIndex + 2}"];
+                text.Focus();
+                text.Select(0, 1);
+            }
+        }
+        private void RandomNumber()
+        {
+            List<int> random_list = new List<int>();
+            for (int i = 0; i < 10; i++) random_list.Add(i);
+            Random random = new Random();
+            int[] int_array = new int[4];
+            for(int i = 0; i < 4; i++)
+            {
+                int index = random.Next(0, random_list.Count);
+                int_array[i] = random_list[index];
+                random_list.RemoveAt(index);
+            }
+            Number = String.Join("", int_array);
+        }
+        private int[] ConvertFromString(string Source)
+        {
+            char[] cha = Source.ToCharArray();
+            int[] output = Array.ConvertAll(cha, x => Convert.ToInt32(x.ToString()));
+            return output;
+        }
+        public int[] Compare(int[] inputnumber,int[] correctnumber)
+        {
+            int A = 0;
+            int B = 0;
+            List<int> input_ = new List<int>(inputnumber);
+            List<int> correct_ = new List<int>(correctnumber);
+            int offset = 0;
+            for (int i = 0; i < inputnumber.Count();i++)
+            {
+                if (inputnumber[i] == correctnumber[i])
+                {
+                    A++;
+                    input_[i] = -1;
+                    correct_[i] = -1;
+                    offset += 1;
+                }
+            }
+            input_.RemoveAll(x => x == -1);
+            correct_.RemoveAll(x => x == -1);
+            B = input_.Intersect(correct_).Count();
+            return new int[] {A,B};
+        }
+        private string ConvertFromIntArray(int[] Source)
+        {
+            return String.Join("", Source);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] inputNumber = ConvertFromString(InputNumber);
+            int[] correctnumber = ConvertFromString(Number);
+            int[] Result = Compare(inputNumber, correctnumber);
+            ListViewItem lvi = new ListViewItem(new string[] {$"{ConvertFromIntArray(inputNumber)}",$"{Result[0]}A{Result[1]}B"});
+            listView1.Items.Add(lvi);
+            if (Result[0] == 4)
+            {
+                MessageBox.Show($"恭喜您，您成功猜對了。正確的數字即為{Number}!", "過關", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Disable();
+            }
+        }
+        private void d_KeyDown(object sender, KeyEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            switch (e.KeyCode)
+            {
+                case Keys.Back:
+                    if(textBox.TabIndex > 0&&textBox.Text.Length==0)
+                    {
+                        TextBox text = (TextBox)panel1.Controls[$"d{textBox.TabIndex}"];
+                        text.Focus();
+                        text.Select(0, 1);
+                    }
+                    break;
+                case Keys.Left:
+                    if (textBox.TabIndex > 0)
+                    {
+                        TextBox text = (TextBox)panel1.Controls[$"d{textBox.TabIndex}"];
+                        text.Focus();
+                        text.Select(0, 1);
+                    }
+                    break;
+                case Keys.Right:
+                    if (textBox.TabIndex < 3)
+                    {
+                        TextBox text = (TextBox)panel1.Controls[$"d{textBox.TabIndex + 2}"];
+                        text.Focus();
+                        text.Select(0, 1);
+                    }
+                    break;
+            }
+        }
+    }
+}
